2035283e5421dab7b0da6f6a5f4b07e2a42206e2
Emilian Bold <emi@apache.org>
2019-07-24 08:32:41 +0300

    Updates info, links, hash for CoolBeans 2019.06

--- Casks/coolbeans.rb
+++ Casks/coolbeans.rb
@@ -1,12 +1,12 @@
 cask 'coolbeans' do
-    version '2018.12'
-    sha256 'ad707c31e3b225de8d67cb961155d998f53b27cbbe4a44d5743997d8bd14c0d5'
+    version '2019.06'
+    sha256 'e8fe059b8572305d7761ee9c224ce2292434239080f8219b8d325758bde5e3c6'
 
-    url "https://paris.download.coolbeans.xyz/download/CoolBeans-#{version}.1.dmg"
+    url "http://download.librebeans.org/download/CoolBeans-#{version}.dmg"
     appcast 'https://coolbeans.xyz/download-macos.html'
     name 'CoolBeans'
     homepage 'https://coolbeans.xyz/'
 
-    app "CoolBeans #{version}.app"
+    app "CoolBeans.app"
 end
   
